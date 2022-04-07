b2c8d24a2080cb74b264f7b3a1d2d11841b61e45
Prashit Prakashbhai Patel <prashitppatel@gmail.com>
2022-04-07 00:35:39 -0300

    added add product lambda

---
+++ backend/add-product/index.js
@@ -0,0 +1,79 @@
+const AWS = require("aws-sdk");
+const Ajv = require("ajv");
+const ajv = new Ajv(); // options can be passed, e.g. {allErrors: true}
+
+const uuid = require("uuid");
+
+module.exports.handler = async (event) => {
+  try {
+    const body = event.data;
+    validateData(body);
+
+    AWS.config.credentials = {
+      accessKeyId: "AKIASVGLKLB6K6EQAUPJ",
+      secretAccessKey: "d6waIXEGNfm4qd3b96U4M+Bx3p9vodhylVYQzVym",
+    };
+    var s3,
+      res = {};
+    body.ItemId = body.ItemName.replace(/\s/g, "") + uuid.v4();
+
+    if (body.Image1) {
+      s3 = new AWS.S3();
+
+      var presignedPUTURL = s3.getSignedUrl("putObject", {
+        Bucket: "s3-bucket-s3bucket-1wjvsaapgbhy2",
+        Key: body.ItemId, //filename
+        Expires: 500, //time to expire in seconds
+      });
+      body.Image1 = presignedPUTURL;
+      res.Image1 = presignedPUTURL;
+    }
+
+    if (body.Image2) {
+      var presignedPUTURL = s3.getSignedUrl("putObject", {
+        Bucket: "s3-bucket-s3bucketingreds-ydw7cqk3z7ig",
+        Key: body.ItemId, //filename
+        Expires: 500, //time to expire in seconds
+      });
+      body.Image2 = presignedPUTURL;
+      res.Image2 = presignedPUTURL;
+    }
+    body.Visible = false;
+    body.ItemName = body.ItemName.toLowerCase();
+    body.ItemType = "#";
+
+    var ddb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
+
+    var params = {
+      TableName: "Items",
+      Item: AWS.DynamoDB.Converter.marshall(body),
+    };
+
+    const data = await ddb.putItem(params).promise();
+    console.log(res);
+    return { statusCode: 200, body: JSON.stringify(res) };
+  } catch (error) {
+    const message = error?.message ? error.message : "Internal server error";
+    return { statusCode: 500, body: JSON.stringify({ message: message }) };
+  }
+};
+
+const validateData = (data) => {
+  const schema = {
+    type: "object",
+    properties: {
+      ItemName: { type: "string" },
+      Image1: { type: "boolean" },
+      Image2: { type: "boolean" },
+      User: { type: "string" },
+      ItemInfo: { type: "string" },
+      Location: { type: "string" },
+    },
+    required: ["ItemName", "User"],
+    additionalProperties: false,
+  };
+  const validate = ajv.compile(schema);
+  const valid = validate(data);
+  if (!valid) throw validate.errors;
+  return true;
+};
